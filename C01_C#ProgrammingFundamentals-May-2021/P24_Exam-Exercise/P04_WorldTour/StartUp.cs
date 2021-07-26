ee6581213673ed54c36d38d088f068b1ba5ac5be
Statev07 <petiostatev2703@gmail.com>
2021-07-26 15:22:12 +0300

    Add tasks.

---
+++ C01_C#ProgrammingFundamentals-May-2021/P24_Exam-Exercise/P04_WorldTour/StartUp.cs
@@ -0,0 +1,108 @@
+﻿namespace P04_WorldTour
+{
+    using System;
+
+    public class StartUp
+    {
+        const string COMMAND_TO_STOP_TRAVEL = "Travel";
+        const string RESULT_MESSAGE = "Ready for world tour! Planned stops: {0}";
+
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                string[] arguments = Console.ReadLine()
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries);
+
+                bool isStopCommand = arguments[0] == COMMAND_TO_STOP_TRAVEL;
+                if (isStopCommand)
+                {
+                    PrintResult(input);
+                    break;
+                }
+
+                string command = arguments[0];
+                input = ExecuteCommands(input, arguments, command);
+            }
+        }
+
+        private static string ExecuteCommands(string input, string[] arguments, string command)
+        {
+            switch (command)
+            {
+                case "Add Stop":
+                    int index = int.Parse(arguments[1]);
+                    string place = arguments[2];
+                    input = Add(input, index, place);
+                    break;
+                case "Remove Stop":
+                    int startIndex = int.Parse(arguments[1]);
+                    int endIndex = int.Parse(arguments[2]);
+                    input = Remove(input, startIndex, endIndex);
+                    break;
+                case "Switch":
+                    string oldPlace = arguments[1];
+                    string newPlace = arguments[2];
+                    input = Switch(input, oldPlace, newPlace);
+                    break;
+            }
+
+            return input;
+        }
+
+        private static string Add(string input, int index, string place)
+        {
+            bool isValid = IsIndexValid(input, index);
+            if (isValid)
+            {
+                input = input.Insert(index, place);
+            }
+            PrintDistanationAfterOperation(input);
+
+            return input;
+        }
+
+        private static string Remove(string input, int startIndex, int endIndex)
+        {
+            bool isStartIndexValid = IsIndexValid(input, startIndex);
+            bool isEndIndexValid = IsIndexValid(input, endIndex);
+
+            if (isStartIndexValid && isEndIndexValid)
+            {
+                int count = endIndex - startIndex;
+
+                input = input.Remove(startIndex, count + 1);
+            }
+            PrintDistanationAfterOperation(input);
+
+            return input;
+        }
+
+        private static string Switch(string input, string oldPlace, string newPlace)
+        {
+            input = input.Replace(oldPlace, newPlace);
+            PrintDistanationAfterOperation(input);
+
+            return input;
+        }
+
+        private static bool IsIndexValid(string input, int index)
+        {
+            bool isValid = input.Length > index && index >= 0;
+
+            return isValid;
+        }
+
+        private static void PrintDistanationAfterOperation(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void PrintResult(string result)
+        {
+            Console.WriteLine(string.Format(RESULT_MESSAGE, result));
+        }
+    }
+}
---
+++ C01_C#ProgrammingFundamentals-May-2021/P24_Exam-Exercise/P05_AdAstra/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace P05_AdAstra
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        const int NEED_KCAL_PER_DAY = 2000;
+
+        const string PATTERN = @"([#]|[|])(?<item>[A-z\s]+)\1(?<expirationdate>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d+)\1";
+
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            int calories = 0;
+            int days = 0;
+
+            Regex regex = new Regex(PATTERN);
+            var matches = regex.Matches(input);
+            calories = CaloriesSum(calories, matches);
+            days = NeedDays(calories, days);
+            PrintResult(days, matches);
+        }
+
+        private static int CaloriesSum(int calories, MatchCollection matches)
+        {
+            for (int index = 0; index < matches.Count; index++)
+            {
+                calories += int.Parse(matches[index].Groups["calories"].Value);
+            }
+
+            return calories;
+        }
+
+        private static int NeedDays(int calories, int days)
+        {
+            while (calories >= NEED_KCAL_PER_DAY)
+            {
+                days++;
+                calories -= NEED_KCAL_PER_DAY;
+            }
+
+            return days;
+        }
+
+        private static void PrintResult(int days, MatchCollection matches)
+        {
+            Console.WriteLine($"You have food to last you for: {days} days!");
+            foreach (Match food in matches)
+            {
+                string foodName = food.Groups["item"].Value;
+                string date = food.Groups["expirationdate"].Value;
+                string calorie = food.Groups["calories"].Value;
+
+                Console.WriteLine($"Item: {foodName}, Best before: {date}, Nutrition: {calorie}");
+            }
+        }
+    }
+}

eae67e9553a8b21e6e7fe26fcde14f62023dac3e
Statev07 <petiostatev2703@gmail.com>
2021-08-13 11:56:53 +0300

    Add task.

--- C02_C#ProgrammingAdvanced-September-2021/P04_MultidimensionalArrays - Exercise/P05_SnakeMoves/StartUp.cs
+++ C02_C#ProgrammingAdvanced-September-2021/P04_MultidimensionalArrays - Exercise/P05_SnakeMoves/StartUp.cs
@@ -15,6 +15,70 @@
             char[,] matrix = new char[matrixSize[0], matrixSize[1]];
 
             matrix = ReadMatrix(matrix);
+            Print(matrix);
+        }
+
+        private static char[,] ReadMatrix(char[,] matrix)
+        {
+            string snake = Console.ReadLine();
+
+            string snakeDate = snake;
+            string direction = "right";
+            string forwardDirection = string.Empty;
+            int snakeCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (direction == "right")
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = snake[snakeCol];
+                        snake = snake.Remove(0, 1);
+                        snake = IsSnakeEmpty(snake, snakeDate);
+                    }
+                    direction = "down";
+                    forwardDirection = "right";
+                    continue;
+                }
+                else if (direction == "down" && forwardDirection == "right")
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[snakeCol];
+                        snake = snake.Remove(0, 1);
+                        snake = IsSnakeEmpty(snake, snakeDate);
+                    }
+                    direction = "right";
+                    continue;
+                }
+
+            }
+
+            return matrix;
+        }
+
+        private static string IsSnakeEmpty(string snake, string snakeDate)
+        {
+            if (snake == string.Empty)
+            {
+                snake = snakeDate;
+            }
+
+            return snake;
+        }
+
+        private static void Print(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string result = string.Empty;
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    result += matrix[row, col];
+                }
+                Console.WriteLine(result);
+            }
         }
     }
 }
